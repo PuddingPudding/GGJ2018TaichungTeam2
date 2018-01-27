7555098b426769f2f49a388ccc93ed0eb0fc8274
PuddingPudding <hwsh70119@gmail.com>
2018-01-27 18:51:41 +0800

    關卡(第一區) 雛形完成

--- Assets/PuddingPudding/GameManager.cs
+++ Assets/PuddingPudding/GameManager.cs
@@ -8,10 +8,10 @@ public class GameManager : MonoBehaviour
 {
     public Collider2D chargingArea1P;
     private SpriteRenderer sprite1P;
-    private p1_control p1Control;
-    private p2_control p2Control;
+    public p1_control p1Control;
     public Collider2D chargingArea2P;
     private SpriteRenderer sprite2P;
+    public p2_control p2Control;
     public Color chargingColor;
     private Color originColor;
     public float transformTime = 0.4f;
@@ -21,10 +21,14 @@ public class GameManager : MonoBehaviour
     public float energyChargeSpeed = 10;
     private bool charging = false;
     public Image energyBar;
+    private bool isGaming = false;
+    public GameObject gameOverText;
 
     // Use this for initialization
     void Start()
     {
+        isGaming = true;
+        gameOverText.SetActive(false);
         currentEnergy = energyMax;
         sprite1P = chargingArea1P.GetComponent<SpriteRenderer>();
         sprite2P = chargingArea2P.GetComponent<SpriteRenderer>();
@@ -34,34 +38,54 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(chargingArea1P.IsTouching(chargingArea2P) )
+        if (p1Control == null || p2Control == null)
         {
-            currentEnergy += energyChargeSpeed * Time.deltaTime;
-            if(currentEnergy > energyMax)
+            isGaming = false;
+            Time.timeScale = 0; //一但有任何一個玩家死掉，就把時間暫停
+            gameOverText.SetActive(true);
+        }
+
+        if (isGaming)
+        {
+            if (chargingArea1P.IsTouching(chargingArea2P))
             {
-                currentEnergy = energyMax;
+                currentEnergy += energyChargeSpeed * Time.deltaTime;
+                if (currentEnergy > energyMax)
+                {
+                    currentEnergy = energyMax;
+                }
+                if (!charging)
+                {
+                    charging = true;
+                    DOTween.To(() => sprite1P.color, x => sprite1P.color = x, chargingColor, transformTime);
+                    DOTween.To(() => sprite2P.color, x => sprite2P.color = x, chargingColor, transformTime);
+                }
             }
-            if(!charging)
+            else
             {
-                charging = true;
-                DOTween.To(() => sprite1P.color, x => sprite1P.color = x, chargingColor, transformTime);
-                DOTween.To(() => sprite2P.color, x => sprite2P.color = x, chargingColor, transformTime);
+                currentEnergy -= energyLostSpeed * Time.deltaTime;
+                if (currentEnergy < 0)
+                {
+                    currentEnergy = 0;
+                }
+                if (charging)
+                {
+                    charging = false;
+                    DOTween.To(() => sprite1P.color, x => sprite1P.color = x, originColor, transformTime);
+                    DOTween.To(() => sprite2P.color, x => sprite2P.color = x, originColor, transformTime);
+                }
+            }
+
+            if (currentEnergy <= 0)
+            {
+                p1Control.Suicide();
+                p2Control.Suicide();
             }
         }
-        else
-        {
-            currentEnergy -= energyLostSpeed * Time.deltaTime;
-            if(currentEnergy < 0)
-            {
-                currentEnergy = 0;
-            }
-            if(charging)
-            {
-                charging = false;
-                DOTween.To(() => sprite1P.color, x => sprite1P.color = x, originColor, transformTime);
-                DOTween.To(() => sprite2P.color, x => sprite2P.color = x, originColor, transformTime);
-            }
-        }
+
+
         energyBar.fillAmount = currentEnergy / energyMax;
+
+        
     }
 }
