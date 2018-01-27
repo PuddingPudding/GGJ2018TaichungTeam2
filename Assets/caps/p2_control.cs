7661ae04688f1769a9fa2a9c65b37fff1762d4a9
DESKTOP-5RJBDDL\caps9 <caps9129@gmail.com>
2018-01-27 13:30:11 +0800

    增加爆炸特效

--- Assets/caps/p2_control.cs
+++ Assets/caps/p2_control.cs
@@ -7,6 +7,7 @@ public class p2_control : MonoBehaviour {
     public float forceValue;
     private Rigidbody2D rigidbody2D = null;
     public float maxSpeed = 7;
+    public GameObject explo; //宣告一個名為explo的物件 
 
     // Use this for initialization
     void Start () {
@@ -44,6 +45,18 @@ public class p2_control : MonoBehaviour {
         }
         Debug.Log(rigidbody2D.velocity.magnitude);
 
+
+
+
+    }
+
+    void OnCollisionEnter2D(Collision2D col) //名為col的觸發事件
+    {
+
+        Destroy(col.gameObject); //消滅碰撞的物件
+        Destroy(gameObject); //消滅物件本身
+        Instantiate(explo, transform.position, transform.rotation); //在外星人的位置產生爆炸
+        Instantiate(explo, col.gameObject.transform.position, col.gameObject.transform.rotation);//在碰撞物件的位置產生爆炸，也就是在太空船的位置產生爆炸
 
     }
 }
