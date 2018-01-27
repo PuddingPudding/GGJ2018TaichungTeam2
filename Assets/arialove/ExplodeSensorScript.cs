3602215c390590c9a53eb36047045e642ae71c5e
AVST30\LTU <arialove841120>
2018-01-27 21:36:45 +0800

    做出隕石爆炸效果 新增tag Border

---
+++ Assets/arialove/ExplodeSensorScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplodeSensorScript : MonoBehaviour {
+
+    public float explodeTime;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Border") //tag為border碰到後觸發
+        {
+            
+            this.GetComponentInParent<MeteoriteScript>().Invoke("MeteoriteExplode", explodeTime);
+        }
+    }
+}
--- Assets/arialove/MeteoriteScript.cs
+++ Assets/arialove/MeteoriteScript.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class MeteoriteScript : MonoBehaviour {
 
@@ -8,6 +9,8 @@ public class MeteoriteScript : MonoBehaviour {
 
     public float rotateSpeed; //旋轉速度
     public GameObject candidate; //會分裂版本的候選人
+    public Color changeColor; //變化後的顏色
+    public float changeTime; //變化的時間
 
     // Use this for initialization
     void Start () {
@@ -28,7 +31,12 @@ public class MeteoriteScript : MonoBehaviour {
 
     public void MeteoriteExplode() //隕石爆炸分裂
     {
-        Instantiate(candidate, this.transform.position, this.transform.rotation);
-        Destroy(this.gameObject);
+        //要爆炸時隕石由原本顏色變成設定顏色後爆炸 並消滅自己本身
+        DOTween.To(() => this.GetComponent<SpriteRenderer>().color, x => this.GetComponent<SpriteRenderer>().color = x, changeColor, changeTime).OnComplete (() =>
+        {
+            Instantiate(candidate, this.transform.position, this.transform.rotation);
+            Destroy(this.gameObject);
+        } );
     }
+
 }
