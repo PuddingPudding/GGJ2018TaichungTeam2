902417ed5b3c141d33e1a1ddac67bd68b8e011ed
DESKTOP-5RJBDDL\caps9 <caps9129@gmail.com>
2018-01-28 11:03:08 +0800

    完成幽浮

--- Assets/caps/BulletScript.cs
+++ Assets/caps/BulletScript.cs
@@ -2,21 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BulletScript : MonoBehaviour {
+public class BulletScript : MonoBehaviour
+{
 
-    
+    Vector3 bulletDirection;
+    public float speed = 4;
 
-    void Start () { 
-
+    void Start()
+    {
+        
     }
-   
-       
 
-    
+
+
 
 
     // Update is called once per frame
-    void Update () {
-        gameObject.transform.position += new Vector3(-0.1f, 0, 0);
+    void Update()
+    {
+        this.GetComponent<Rigidbody2D>().velocity = this.transform.right * speed;
+        gameObject.transform.position += bulletDirection;
+        InvokeRepeating("Suicide", 3.0f, 3.0f);
+    }
+
+    public void Suicide()
+    {
+        Destroy(gameObject); //消滅物件本身
     }
 }
+
